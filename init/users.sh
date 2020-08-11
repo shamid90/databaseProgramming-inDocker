c5006be18477c0f0557f2e575e2596035c367e16
SHamid90 <hr90arabi@gmail.com>
2020-08-11 16:19:36 +0200

    Complete users.sh for LMS.sql and Bash Scripts

--- init/users.sh
+++ init/users.sh
@@ -15,3 +15,15 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT pg_read_server_files TO $user;
     ALTER USER $user WITH SUPERUSER;
 EOSQL
+
+bash /docker-entrypoint-initdb.d/sample/RandomStudents.sh &
+bash /docker-entrypoint-initdb.d/sample/RandomInstructor.sh &
+bash /docker-entrypoint-initdb.d/sample/RandomCourse.sh &
+bash /docker-entrypoint-initdb.d/sample/RandomCourseContent.sh &
+bash /docker-entrypoint-initdb.d/sample/RandomRegistration.sh &
+wait
+
+
+
+    echo Creating LMS sample database
+    echo $user_pw | psql -W --username $user --dbname $user -f /docker-entrypoint-initdb.d/sample/LMS.sql
